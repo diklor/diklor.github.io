866ac6e58498ffd4766ef5f947d920faf18298bc
unknown <guyueege8090@gmail.com>
2024-05-13 16:21:09 +0300

    popup

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function delete_button() {
     var element = document.getElementById('0')
     element.parentNode.removeChild(element)
 }
+function toggle_popup() {
+    var popup = document.getElementById('popup-1')
+    popup.classList.toggle('active');
+}
